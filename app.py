485faa5ba7a371c56776f33b1e3bec82a0194588
ConstructoraCRV <comprasconstructoracrv@gmail.com>
2026-06-01 09:00:35 -0500

    Corregir puerto Railway

--- app.py
+++ app.py
@@ -181,5 +181,13 @@ def crear_app():
 
 app = crear_app()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
