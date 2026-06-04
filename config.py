3bd3b4eab652b079dfa55822430afccaa64e0f0b
ConstructoraCRV <comprasconstructoracrv@gmail.com>
2026-06-04 10:05:26 -0500

    Corregir conexion PostgreSQL con pg8000

--- config.py
+++ config.py
@@ -3,10 +3,15 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SECRET_KEY = "clave-secreta-inventario-obra"
 
     database_url = os.getenv("DATABASE_URL")
 
     if database_url:
+        database_url = database_url.replace(
+            "postgresql://",
+            "postgresql+pg8000://"
+        )
         SQLALCHEMY_DATABASE_URI = database_url
     else:
         SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "inventario_obra.db")
